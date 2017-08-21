a64e93fcc03aac72aef4918ba5b054cb2384f5c8
Your Name <Nicholas94920@gmail.com>
2017-08-21 06:02:38 +0000

    Added New Nvidia Script and Model

--- clone.py
+++ clone.py
@@ -2,41 +2,30 @@ import csv
 import cv2
 import numpy as np
 
-lines = []
-with open('../data1/driving_log.csv') as csvfile:
+lines = [] 
+with open('../data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
-		lines.append(line)
+		lines.append(line) 
 
-images = []
-measurements =[]
+images=[]
+measurments = []
 for line in lines:
-	for i in range(3):
-		source_path = line[i]
-		filename = source_path.split('/')[-1]
-		current_path = '../data1/IMG/' + filename
-		image = cv2.imread(current_path)
-		images.append(image)
-		measurement = float(line[3])
-		measurements.append(measurement)
+	source_path = line[0]
+	filename = source_path.split('/')[-1]
+	current_path = '../data/IMG/' + filename
+	image = cv2.imread(current_path) 
+	image.append(image)
+	measurement = float(line[3])
+	measuremnets.append(measurement)
 
-X_train = np.array(images)
+X_train = np.array(images) 
 y_train = np.array(measurements)
 
-print('X_train shap:', X_train.shape)
-print('y_train shap:', y_train.shape)
-
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keraslayers import Flatten, Dense 
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255 - 0.5, input_shape=(160,320,3)))
-model.add(Flatten())
+model.add(Flatten(input_shap=(160,320,3)))
 model.add(Dense(1))
-
-model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
-
-model.save('model.h5')
-exit()
 
--- nvidia.py
+++ nvidia.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 lines = []
-with open('../data5/driving_log.csv') as csvfile:
+with open('../data2/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
@@ -14,18 +14,25 @@ for line in lines:
 	for i in range(3):	
 		source_path = line[i]
 		filename = source_path.split('/')[-1]
-		current_path = '../data5/IMG/' + filename
+		current_path = '../data2/IMG/' + filename
 		image = cv2.imread(current_path)
 		images.append(image)
-		measurement = float(line[3])
-		measurements.append(measurement)
+	correction = 0.05
+	measurement = float(line[3])
+	measurements.append(measurement)
+	measurements.append(measurement+correction)
+	measurements.append(measurement-correction)		
 
-augmented_images, augmented_measuremnets = [], []
+
+augmented_images = []
+augmented_measurements = []
 for image, measurement in zip(images, measurements):
 	augmented_images.append(image)
-	augmented_measuremnets.append(measurement)
-	augmented_images.append(cv2.flip(image,1))
-	augmented_measuremnets.append(measurement*-1)
+	augmented_measurements.append(measurement)
+	flipped_image = cv2.flip(image, 1)
+	flipped_measurement = float(measurement) * -1.0 
+	augmented_images.append(flipped_image)
+	augmented_measurements.append(flipped_measurement)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -54,5 +61,5 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
-model.save('model.h5')
+model.save('nvidia.h5')
 exit()
